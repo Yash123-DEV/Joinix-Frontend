43f90e71e243d4268cdfc5ea25e94ad976a4e9b4
Yash123-DEV <shubhrajkhatri123@gmail.com>
2025-07-02 20:23:47 +0530

    make changes app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,8 @@
 "use client";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkSession = async () => {
-      try {
-        const res = await fetch("https://joinix-backend1.onrender.com/api/auth/me",{
-          credentials: "include",
-        });
-
-        if(res.ok) {
-          const data = await res.json();
-          console.log("User data:", data);
-          router.push("/dashboard");
-        }else {
-          router.push("/sign-in");
-        }
-      }catch (error) {
-        console.error("Error checking session:", error);
-        router.push("/sign-up");
-      }
-    }
-    checkSession();
-  }, [router]);
 
   return (
     <div className="text-center mt-10">Checking login status...</div>
